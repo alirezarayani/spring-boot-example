4045c49309459b38a1b52d7bf63f4a9cf8f6a33a
Alireza <alireza.rayani@gmail.com>
2024-02-13 21:29:28 +0330

    Init project

---
+++ src/main/java/ir/lazydeveloper/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package ir.lazydeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExampleApplication.class, args);
+    }
+
+}
